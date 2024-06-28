a49cdfca4fdf75f93b9869b45eb786cd02b00ee5
fmchtt <felipehmicheletto@gmail.com>
2024-06-28 10:07:31 -0300

    Add permission validation on company update

--- Workshop.UI/src/routes/_protected/management/company.lazy.tsx
+++ Workshop.UI/src/routes/_protected/management/company.lazy.tsx
@@ -4,12 +4,20 @@ import {
   RouteContainer,
   SideContainer,
 } from "../../../components/styles.global";
+import PendingComponent from "../../../components/pendingComponent";
+import usePermissions from "../../../hooks/usePermissions";
 
 export const Route = createLazyFileRoute("/_protected/management/company")({
   component: UpdateCompany,
 });
 
 function UpdateCompany() {
+  const validatingPermission = usePermissions();
+
+  if (validatingPermission([{ type: "management", value: "manageCompany" }])) {
+    return <PendingComponent />;
+  }
+
   return (
     <RouteContainer>
       <SideContainer>
